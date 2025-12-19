9d4a062725e89db72f462cf0986926e6918df53d
marCoderApp <farbusfer98@gmail.com>
2025-12-19 09:25:29 -0300

    First commit

---
+++ src/main/java/com/project/apibookshop/ApibookshopApplication.java
@@ -0,0 +1,13 @@
+package com.project.apibookshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApibookshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApibookshopApplication.class, args);
+	}
+
+}
