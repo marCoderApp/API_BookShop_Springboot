acf03f0859ad7c67a942d7e10f3f8540088a7076
marCoderApp <farbusfer98@gmail.com>
2026-01-04 22:52:32 -0300

    ADD - Security config update for authors endpoints.

--- src/main/java/com/project/apibookshop/security/SecurityConfig.java
+++ src/main/java/com/project/apibookshop/security/SecurityConfig.java
@@ -62,6 +62,11 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.PUT,"/api/books/**").hasAnyRole("SUPER_ADMIN","ADMIN", "LIBRARIAN")
                         .requestMatchers(HttpMethod.PATCH,"/api/books/**").hasAnyRole("SUPER_ADMIN","ADMIN", "LIBRARIAN")
                         .requestMatchers(HttpMethod.DELETE,"/api/books/**").hasAnyRole("SUPER_ADMIN","ADMIN", "LIBRARIAN")
+                        .requestMatchers(HttpMethod.GET,"/api/authors").permitAll()
+                        .requestMatchers(HttpMethod.PATCH,"/api/authors/**").hasAnyRole("SUPER_ADMIN","ADMIN", "LIBRARIAN")
+                        .requestMatchers(HttpMethod.POST,"/api/authors/**").hasAnyRole("SUPER_ADMIN","ADMIN", "LIBRARIAN")
+                        .requestMatchers(HttpMethod.DELETE,"/api/authors/**").hasAnyRole("SUPER_ADMIN","ADMIN", "LIBRARIAN")
+                        .requestMatchers(HttpMethod.PUT,"/api/authors/**").hasAnyRole("SUPER_ADMIN","ADMIN", "LIBRARIAN")
                         .requestMatchers(HttpMethod.GET,"/api/genre/**").permitAll()
                         .requestMatchers(HttpMethod.GET,"/api/genre/**").permitAll()
                         //USERS
